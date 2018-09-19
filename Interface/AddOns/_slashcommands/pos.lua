9097acc9d64e990b7076e176da07770c15312a93
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2018-09-19 14:24:54 +0200

    Change table syntax to be consistent

--- Interface/AddOns/_slashcommands/pos.lua
+++ Interface/AddOns/_slashcommands/pos.lua
@@ -1,5 +1,5 @@
-SlashCmdList.POS = function()
+SlashCmdList["POS"] = function()
 	local p = C_Map.GetPlayerMapPosition(C_Map.GetBestMapForUnit("player"), "player")
-	DEFAULT_CHAT_FRAME:AddMessage(format("( %s ) %.1f,%.1f", GetZoneText(), p.x * 100, p.y * 100))
+	DEFAULT_CHAT_FRAME:AddMessage(format("[%s] %.1f, %.1f", GetZoneText(), p.x * 100, p.y * 100))
 end
 SLASH_POS1 = "/pos"
