c5d63b331cb31ab3101bc31b8b7308bf424d0f03
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2018-10-01 20:13:48 +0200

    Remplace unit name by arena number on nameplate

--- Interface/AddOns/_nameplates/core.lua
+++ Interface/AddOns/_nameplates/core.lua
@@ -6,3 +6,15 @@ NamePlateDriverFrame.OnUnitAuraUpdate = function() end
 -- InterfaceOptionsNamesPanelUnitNameplatesMakeLarger.largeHorizontalScale = 1.2   -- default: 1.4
 -- InterfaceOptionsNamesPanelUnitNameplatesMakeLarger.normalVerticalScale = 1      -- default: 1.0
 -- InterfaceOptionsNamesPanelUnitNameplatesMakeLarger.largeVerticalScale = 1.6     -- default: 2.7
+
+hooksecurefunc("CompactUnitFrame_UpdateName", function(frame)
+    if IsActiveBattlefieldArena() and frame.unit:find("nameplate") then
+        for i = 1, 5 do
+            if UnitIsUnit(frame.unit, "arena".. i) then
+                frame.name:SetText(i)
+                frame.name:SetTextColor(1,1,0)
+                break
+            end
+        end
+    end
+end)
