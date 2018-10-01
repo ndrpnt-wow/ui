12fd445aed007066591016031bd002fce75ef4a6
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2018-10-01 20:38:04 +0200

    Move loss of control alert up

--- Interface/AddOns/_lossofcontrol/core.lua
+++ Interface/AddOns/_lossofcontrol/core.lua
@@ -7,7 +7,7 @@ LossOfControlFrame.TimeLeft.NumberText:Hide()
 LossOfControlFrame.TimeLeft.SecondsText:Hide()
 LossOfControlFrame.Cooldown:SetDrawEdge(false)
 hooksecurefunc("LossOfControlFrame_SetUpDisplay", function(self)
-	self.Icon:SetPoint("CENTER", 0, -160);
+	self.Icon:SetPoint("CENTER", 0, -80);
 end)
 hooksecurefunc("CooldownFrame_Set", function(self)
 	if self.currentCooldownType == COOLDOWN_TYPE_LOSS_OF_CONTROL then
